416d1de4b4e3dd82e0d4c7964b1820bda27d3207
brenton.white <brentonwhite93@gmail.com>
2020-05-14 20:50:06 -0400

    added script to removed previouse grid before creating new grid

--- myscripts.js
+++ myscripts.js
@@ -7,7 +7,7 @@ for (i = 1; i <= e; i++){
   const div = document.createElement('div');
   div.setAttribute( 'style', 'background-color: red;');
   div.classList.add('box'); 
-  div.addEventListener('mouseover', () => {
+  div.addEventListener('mouseover', () => { // look that this
     div.setAttribute( 'style', 'background-color: black;');
   });
   etch.appendChild(div);
@@ -19,11 +19,17 @@ createGrid(grid)
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
 
-    const box = document.querySelectorAll('.box');
+   /* const box = document.querySelectorAll('.box');
     box.forEach((div) => {
-    div.setAttribute( 'style', 'background-color: red;'); });
+    div.setAttribute( 'style', 'background-color: red;'); }); */
+
+    let element = document.getElementById("etch");
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
 
     let newGrid = prompt("How many squares per side for the new grid?")
+    
 
     const etch = document.querySelector('#etch')
     etch.style.setProperty('--grid', newGrid);
